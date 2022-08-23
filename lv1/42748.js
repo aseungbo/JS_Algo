d2c5c8e5080868f4f1af65f6422f33ab2113eb8b
aseungbo <seungbo1112@gmail.com>
2022-08-23 11:16:00 +0900

    Add files

---
+++ lv1/42748.js
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42748
+
+function solution(array, commands) {
+    var answer = [];
+
+    for (let i = 0; i < commands.length; i++)
+    {
+        var arr = array.slice(commands[i][0] - 1, commands[i][1]);
+        answer.push(arr.sort((a, b) => a - b)[commands[i][2] - 1]);
+    }     
+    return answer;
+}
