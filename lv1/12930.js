c7516202fcaecbe8e574e50e73cbc31ffd5f9556
aseungbo <seungbo1112@gmail.com>
2022-08-24 11:44:03 +0900

    Add 12930 12950 12969 repeat map

---
+++ lv1/12930.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12930
+
+function solution(s) {
+    return s.split(' ').map((ele) => ele.split('').map((val, key) => (key % 2 === 0 ? val.toUpperCase() : val.toLowerCase())).join('')).join(' ');
+}
