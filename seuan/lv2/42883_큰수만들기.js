f778b9dd759dd5c95e22d6f432872760aa9e5331
seuan <seuan@seuanui-MacBookPro.local>
2022-09-08 20:29:41 +0900

    ❌: 42883 stack 다시 이해하기

--- seuan/lv2/42883_큰수만들기.js
+++ seuan/lv2/42883_큰수만들기.js
@@ -1,4 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42883
+// ToDo: 42885와 비교하여 단순히 stack 문제로 해결할 시 발생하는 문제 생각해보기
 function solution(number, k) {
     const arr = number.split('');
     
