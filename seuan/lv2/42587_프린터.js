9799066a6c3a806a7b92ff52d3dba79d60d9a2fd
seungb5 <seungbo1112@gmail.com>
2023-02-13 17:11:01 +0900

    feat: lv2 dequeue

--- seuan/lv2/42587_프린터.js
+++ seuan/lv2/42587_프린터.js
@@ -1,14 +1,41 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/42587
+class Dequeue {
+  constructor(arr = []) {
+    this.queue = arr;
+    this.length = arr.length;
+  }
+  enqueueLast(data) {
+    this.queue.push(data);
+    this.length++;
+  }
+  dequeueFirst() {
+    this.printedCnt++;
+    this.length--;
+    return this.queue.shift();
+  }
+  size() {
+    return this.length;
+  }
+}
 
 function solution(priorities, location) {
-    let stack = [];
-    let poped = [];
-    priorities.map((e, idx) => {
-        while (e > stack[stack.length -1])
-            poped.push(stack.pop());
-        stack.push(idx);
-    })
-    for (let i = 0; i < poped.length; i++)
-        stack.push(poped[i]);
-    return (stack.indexOf(location) + 1);
+  const dequeue = new Dequeue(priorities);
+  const sortedPriorities = priorities.slice().sort((a, b) => a - b);
+  let currLocation = location;
+  let printedCnt = 0;
+
+  while (true) {
+    if (dequeue.size() === 0) break;
+    if (dequeue.queue[0] < sortedPriorities.at(-1)) {
+      dequeue.enqueueLast(dequeue.dequeueFirst());
+      if (currLocation === 0) currLocation = dequeue.size() - 1;
+      else currLocation--;
+    } else {
+      dequeue.dequeueFirst();
+      sortedPriorities.pop();
+      printedCnt++;
+      if (currLocation === 0) break;
+      currLocation--;
+    }
+  }
+  return printedCnt;
 }
